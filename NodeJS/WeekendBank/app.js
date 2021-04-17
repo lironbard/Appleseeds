0daaf0dccf276a98977d2652ecb86ff94452951d
unknown <lironbard@gmail.com>
2021-04-17 17:36:38 +0300

    Updating Node weekend BankAPI exercise files

--- NodeJS/WeekendBank/app.js
+++ NodeJS/WeekendBank/app.js
@@ -32,10 +32,55 @@ app.get("/api/users/:id", (req, res) => {
 //----------END----------------\\
 
 //-----------Update credit------\\
-app.put("/api/users/:id", (req, res) => {
+app.put("/api/users/:id/credit", (req, res) => {
   try {
     updateCredit(req.params.id, req.body);
     res.status(200).send("User updated");
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+//----------END----------------\\
+
+//---Add users---\\
+app.post("/api/users", (req, res) => {
+  try {
+    addUser(req.body);
+    res.status(200).send("User add");
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+//----------END----------------\\
+
+//---Deposit cash to a User---\\
+app.put("/api/users/:id/deposit", (req, res) => {
+  try {
+    console.log(req.params);
+    deposit(req.params.id, req.body);
+    res.status(200).send("Deposit success");
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+//----------END----------------\\
+
+//---Withdraw money from the user with cash---\\
+app.put("/api/users/:id/withdraw", (req, res) => {
+  try {
+    withdrawMoney(req.params.id, req.body);
+    res.status(200).send("withdraw success");
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
+//----------END----------------\\
+
+//---Transferring money from one usr to another---\\
+app.put("/api/users/transfer/:from/:to", (req, res) => {
+  try {
+    transferMoney(req.params.from, req.params.to, req.body);
+    res.status(200).send("Transfer success");
   } catch (e) {
     res.status(400).send({ error: e.message });
   }
--- NodeJS/WeekendBank/utils.js
+++ NodeJS/WeekendBank/utils.js
@@ -29,34 +29,106 @@ const getUser = (id) => {
 //-----------END-----------\\
 
 //---Adding a User---\\
-const addUser = () => {};
+const addUser = (user) => {
+  let users = loadUsers();
+
+  if (users.find((el) => el.passport_id === user.passport_id)) {
+    throw new Error("Passport ID already exist!!");
+  }
+
+  user.credit = Number(user.credit);
+  if (Number.isNaN(user.credit) || user.credit < 0) {
+    throw new Error("Credit needs to be a positive number or exactly 0");
+  }
+
+  user.cash = Number(user.cash);
+  if (Number.isNaN(user.cash) || user.cash < -user.credit) {
+    throw new Error("Cash cant be lower than your credit");
+  }
+
+  users.push(user);
+  saveUsers(users);
+};
 //-----------END-----------\\
 
 //---Deposit cash to a User---\\
-const deposit = () => {};
+const deposit = (id, { amount }) => {
+  const users = loadUsers();
+  let user = users.find((el) => el.passport_id === id);
+  if (!user) throw new Error("User not found");
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) {
+    throw new Error("Amount needs to be a positive number");
+  }
+  user.cash += amount;
+  saveUsers(users);
+};
 //-----------END-----------\\
 
 //---Update e users credit---\\
 const updateCredit = (id, { credit }) => {
-  let users = loadUsers();
+  const users = loadUsers();
   let user = users.find((el) => el.passport_id === id);
   if (!user) throw new Error("User not found");
-  user = Object.assign(user, {
-    credit: credit ? credit : user.credit,
-  });
-  saveUsers(user);
+  user.credit = credit;
+  saveUsers(users);
 };
-const saveUsers = (user) => {
-  fs.writeFileSync(__dirname + "/users.json", JSON.stringify(user));
+const saveUsers = (users) => {
+  try {
+    fs.writeFileSync(__dirname + "/users.json", JSON.stringify(users));
+  } catch (e) {
+    throw new Error(e.message);
+  }
 };
 //-----------END-----------\\
 
 //---Withdraw money from the user with cash---\\
-const withdrawMoney = () => {};
+const withdrawMoney = (id, { amount }) => {
+  const users = loadUsers();
+  let user = users.find((el) => el.passport_id === id);
+  if (!user) throw new Error("User not found");
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) {
+    throw new Error("Amount needs to be a positive number");
+  }
+
+  if (user.cash - amount < -user.credit) {
+    throw new Error("Cash cant be lower than your credit");
+  }
+  user.cash -= amount;
+
+  saveUsers(users);
+};
 //-----------END-----------\\
 
 //---Transferring money from one usr to another---\\
-const transferMoney = () => {};
+const transferMoney = (sending, receiving, { amount }) => {
+  const users = loadUsers();
+  let sendingUser = users.find((el) => el.passport_id === sending);
+  if (!sendingUser) throw new Error("User not found");
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) {
+    throw new Error("Amount needs to be a positive number");
+  }
+
+  if (sendingUser.cash - amount < -sendingUser.credit) {
+    throw new Error("Cash cant be lower than your credit");
+  }
+  sendingUser.cash -= amount;
+
+  //----\\
+
+  let receivingUser = users.find((el) => el.passport_id === receiving);
+  if (!receivingUser) throw new Error("User not found");
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) {
+    throw new Error("Amount needs to be a positive number");
+  }
+  receivingUser.cash += amount;
+
+  saveUsers(users);
+};
+
 //-----------END-----------\\
 
 module.exports = {
