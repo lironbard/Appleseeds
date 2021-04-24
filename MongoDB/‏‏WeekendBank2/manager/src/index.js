d8550d85a4dac8823515888a4172baa991a0d631
unknown <lironbard@gmail.com>
2021-04-24 23:33:29 +0300

    Updating Bank MongoDB project files

--- MongoDB/‏‏WeekendBank2/manager/src/index.js
+++ MongoDB/‏‏WeekendBank2/manager/src/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
+const app = express();
+const cors = require("cors");
 //const Account = require("./models/accounts");
 const userRouter = require("./routers/user");
+const accountRouter = require("./routers/account");
 
-const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
+app.use(accountRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port" + port);
