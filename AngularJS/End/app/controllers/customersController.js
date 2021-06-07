7982093413120a305a2a9f24fd0159638ad30a1e
lironbard <lironbard@gmail.com>
2021-06-07 18:48:36 +0300

    Adding Bootstrap

--- AngularJS/End/app/controllers/customersController.js
+++ AngularJS/End/app/controllers/customersController.js
@@ -1,6 +1,6 @@
 //Option 3
 (function () {
-  var CustomersController = function ($scope, customersFactory, appSettings) {
+  var CustomersController = function ($scope, $log, $window, customersFactory, appSettings) {
     $scope.sortBy = "name";
     $scope.reverse = false;
     $scope.customers = [];
@@ -16,14 +16,36 @@
           data.log(data.error + "" + status);
         });
     }
+    init();
 
     $scope.doSort = function (propName) {
       $scope.sortBy = "propName";
       $scope.reverse = !scope.reverse;
     };
+
+    $scope.deleteCustomer = function (customerId) {
+      customersFactory.deleteCustomer(customerId).then(
+        function (response) {
+          var status = response.data;
+          if (status) {
+            for (var i = 0, len = $scope.customers.length; i < len; i++) {
+              if ($scope.customers[i].id === customerId) {
+                $scope.customers.splice(i, 1);
+                break;
+              }
+            }
+          } else {
+            $window.alert("Unable to delete customer");
+          }
+        },
+        function (data, status, headers, config) {
+          $log.log(data.error + " " + status);
+        }
+      );
+    };
   };
 
-  CustomersController.$inject = ["$scope", "customersFactory", "appSettings"];
+  CustomersController.$inject = ["$scope", "$log", "$window", "customersFactory", "appSettings"];
 
   angular.module("customersApp").controller("CustomerController", CustomersController);
 })();
--- AngularJS/End/server.js
+++ AngularJS/End/server.js
@@ -26,6 +26,31 @@ app.get("/customers", function (req, res) {
   res.json(500, { error: "An error has occurred!" });
 });
 
+app.get("/orders", function (req, res) {
+  var orders = [];
+  for (var i = 0, len = customers.length; i < len; i++) {
+    if (customers[i].orders) {
+      for (var j = 0, ordersLen = customers[i].orders.length; j < ordersLen; j++) {
+        orders.push(customers[i].orders[j]);
+      }
+    }
+  }
+  res.json(orders);
+});
+
+app.delete("/customers/:id", function (req, res) {
+  var customerId = parseInt(req.params.id);
+  var data = { status: true };
+  for (var i = 0, len = customers.length; i < len; i++) {
+    if (customers[i].id === customerId) {
+      customers.splice(i, 1);
+      data = { status: true };
+      break;
+    }
+  }
+  res.json(data);
+});
+
 app.listen(8080);
 
 console.log("Express listening on port 8080");
