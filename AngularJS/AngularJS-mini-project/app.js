5096ba03fae104d25f44c96e68ef8a9a235db99f
lironbard <lironbard@gmail.com>
2021-06-12 13:52:13 +0300

    Adding Controllers , Scripts and Route usage

--- AngularJS/AngularJS-mini-project/app.js
+++ AngularJS/AngularJS-mini-project/app.js
@@ -1,11 +1,31 @@
-var app = angular.module("myList", []);
-app.controller("myListController", function ($scope) {
-  $scope.items = ["Box", "Ball", "Crowbar", "Pen"];
-  $scope.newItem = "";
-  if ($scope.newItem != "") {
-    $scope.pushItem = function () {
-      $scope.items.push($scope.newItem);
-      $scope.newItem = "";
-    };
-  }
+var app = angular.module("myList", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      controller: "myListController",
+      templateUrl: "views/myList.html",
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
 });
+
+// app.controller("myListController", [
+//   "$scope", // Instead of using $inject
+//   function ($scope) {
+//     $scope.items = ["Box", "Ball", "Crowbar", "Pen"];
+//     $scope.newItem = "";
+
+//     $scope.pushItem = function () {
+//       if ($scope.newItem !== "") {
+//         $scope.items.push($scope.newItem);
+//         $scope.newItem = "";
+//       }
+//     };
+
+//     $scope.deleteItem = function (index) {
+//       $scope.items.splice(index, 1);
+//     };
+//   },
+// ]);
---
+++ AngularJS/AngularJS-mini-project/controllers/myListController.js
@@ -0,0 +1,18 @@
+app.controller("myListController", [
+  "$scope", // Instead of using $inject
+  function ($scope) {
+    $scope.items = ["Box", "Ball", "Crowbar", "Pen"];
+    $scope.newItem = "";
+
+    $scope.pushItem = function () {
+      if ($scope.newItem !== "") {
+        $scope.items.push($scope.newItem);
+        $scope.newItem = "";
+      }
+    };
+
+    $scope.deleteItem = function (index) {
+      $scope.items.splice(index, 1);
+    };
+  },
+]);
